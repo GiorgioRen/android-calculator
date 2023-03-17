658e12e176dd4c8304e018a65a05c680c2c0670a
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-03-17 22:28:31 +0100

    Added fade animation if result is number displayed

--- app/src/main/java/com/example/calculator01/Display.java
+++ app/src/main/java/com/example/calculator01/Display.java
@@ -1,6 +1,7 @@
 package com.example.calculator01;
 
 import android.util.Log;
+import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import java.util.Locale;
 public class Display {
 
     private final TextView display;
+    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+    protected AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ;
 
     public Display(@NonNull TextView display){
         this.display = display;
@@ -21,6 +24,16 @@ public class Display {
         Log.i("Display","Set display to "+number);
     }
     public void setDisplayFormatted(CharSequence number){
+        // quick fade out and fade in if result is the same number as the secondNumber (like 12-6=6)
+        if (String.format((String) number).replaceAll("\\.0+$", "").equals((String) display.getText())) {
+            display.startAnimation(fadeOut);
+            display.startAnimation(fadeIn);
+            fadeIn.setDuration(100);
+            fadeIn.setFillAfter(true);
+            fadeOut.setDuration(100);
+            fadeOut.setFillAfter(true);
+            fadeOut.setStartOffset(fadeIn.getStartOffset());
+        }
         // very fancy
         display.setText(String.format(Locale.getDefault(), (String) number).replaceAll("\\.0+$", ""));
         Log.i("Display","Set display formatted to "+number+" removing trailing zeros");
